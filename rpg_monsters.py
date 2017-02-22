0ff20da0bc4673c99231cde904e33cafbba8a33a
Paul <dwkang@uga.edu>
2017-02-22 12:05:13 -0500

    first commit

---
+++ rpg_monsters.py
@@ -0,0 +1,13 @@
+class Goblin(object):
+	def __init__(self):
+		self.name = 'goblin';
+		self.health = 6;
+		self.power = 2;
+	def alive(self):
+		return self.health > 0;
+	def take_damage(self, points_of_damage):
+		self.health -= points_of_damage;
+	def attack(self, hero):		
+		print "%s attacks %s" % (self.name, hero.name)
+		hero.take_damage(self.power);		
+		
